ed6e381c01c3636bb30b7664038dfdbf2c49e945
Alexandria Marbury <93114669+Dree0313@users.noreply.github.com>
2026-01-29 11:01:16 -0800

    Update queries.sql

--- 06_projects/Member Account and Transactions Dashboard/queries.sql
+++ 06_projects/Member Account and Transactions Dashboard/queries.sql
@@ -6,5 +6,13 @@ Tier 1 - Core Querying
   -- 2. Select specific colums instead of Select *
     SELECT first_name FROM Members;
   -- 3. Use column aliases (AS) for readability
-    SELECT first_name AS 'First Name' FROM Members;
-    
+    SELECT first_name AS "First Name" FROM Members;
+
+-- Filtering with WHERE
+  -- 1. Filter members by active status
+     SELECT member_id FROM Members WHERE status IS 'Active';
+  -- 2. Apply comparison operators (=, !=)
+    SELECT member_id FROM Members WHERE status = 'Active';
+    SELECT member_id FROM Members WHERE status != 'Active';
+
+-- Sorting & Limiting
