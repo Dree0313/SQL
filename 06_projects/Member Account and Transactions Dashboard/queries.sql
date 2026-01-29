1b347084c5c45feb3c22fdb7a81535d8356bd74a
Alexandria Marbury <93114669+Dree0313@users.noreply.github.com>
2026-01-29 10:42:20 -0800

    Create queries.sql

---
+++ 06_projects/Member Account and Transactions Dashboard/queries.sql
@@ -0,0 +1,10 @@
+Tier 1 - Core Querying
+
+-- Basic SELECT Usage
+  -- 1. List all members
+    SELECT * FROM Members;
+  -- 2. Select specific colums instead of Select *
+    SELECT first_name FROM Members;
+  -- 3. Use column aliases (AS) for readability
+    SELECT first_name AS 'First Name' FROM Members;
+    
