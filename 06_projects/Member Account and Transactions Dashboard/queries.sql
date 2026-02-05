813438280a77c06009da87e0446994308eaeb068
Alexandria Marbury <93114669+Dree0313@users.noreply.github.com>
2026-02-05 15:43:37 -0800

    Update queries.sql

--- 06_projects/Member Account and Transactions Dashboard/queries.sql
+++ 06_projects/Member Account and Transactions Dashboard/queries.sql
@@ -148,7 +148,7 @@ Tier 1
       107         5          Checking      1200.0   2023-12-25
 
 -- Aggregate Functions
-  -- 1. Total balance per member (SUM)
+  -- 1. Total balance for all member accounts (SUM)
 
   -- 2. Average transaction amount per account (AVG)
   -- 3. Count of transactions per account (Count)
