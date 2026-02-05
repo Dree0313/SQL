aebf73b7b099aec942cde02fd4f17f581eecd979
Alexandria Marbury <93114669+Dree0313@users.noreply.github.com>
2026-02-05 09:34:29 -0800

    Update 01_COUNT.sql

--- 01_beginner/04_Simple_Aggregations/01_COUNT.sql
+++ 01_beginner/04_Simple_Aggregations/01_COUNT.sql
@@ -1,13 +1,12 @@
 __________________________________________________________________________
--- Beginner SQL: Using WHERE Safely with Write Operations
--- Purpose: Learn how to safely use WHERE with INSERT, UPDATE, and DELETE
+-- Beginner SQL: COUNT
+-- Purpose: Learn how to count rows in SQL queries
 __________________________________________________________________________
 
 -- Scenerio:
   -- You are a junior database developer at a company. Managment needs 
-  -- changes made to employee data, but mistakes can cause permanent data
-  -- loss. You must use WHERE safely when performing write opertations 
-  -- (UPDATE and DELETE).
+  -- reports on totals, like how many employees are active, in a certain
+  -- department, or have missing data. You will use COUNT to measure rows.
 
   -- Table: employees
     -- id            1       2          3          4          5
@@ -19,166 +18,100 @@ __________________________________________________________________________
     -- status      Active   Active    Inactive   Active      Active
   
 __________________________________________________________________________
--- 1️ ALWAYS Preview with SELECT First
--- What it does: Shows exactly which rows will be affected
--- Why use it: Prevents accidental mass updates or deletes
--- Rule: If you wouldn't run it as SELECT, don't run it as UPDATE/DELETE
+-- 1️ Basic COUNT
+-- What it does: Counts the number of rows in a table
+-- Why use it: Quickly finds out how many records exist
+-- Syntax: SELECT COUNT(*) FROM table_name;
 __________________________________________________________________________
 -- Problem: 
-  -- Management wants to delete Carol
+  -- Management wants to know how many employees exist
 
 -- Solution: 
-  SELECT *
-    FROM employees
-    WHERE first_name = 'Carol' AND last_name = 'Davis';
-
-DELETE FROM employees
-  WHERE first_name = 'Carol' AND last_name = 'Davis';
+  SELECT COUNT(*) AS TOTALEmployees
+    FROM employees;
 
 -- Expected Results:
-  -- id            1       2          4          5
-  -- first_name  Alice     Bob       Dave       Eve
-  -- last_name   Johnson  Smith      Wilson     Taylor
-  -- department    HR      IT         IT       Finance
-  -- role        Manager  Analyst  Developer  Accountant
-  -- salary       75000    60000    65000        NULL
-  -- status      Active   Active    Active      Active
+  -- TotalEmployees  5
 
 __________________________________________________________________________
--- 2 Use Multiple Conditions (Be Specific)
--- What it does: Narrows down affected rows
--- Why use it: Reduces risk of deleting or updating the wrong data
--- Best Practice: Combine columns like name + department or id
+-- 2 COUNT with WHERE
+-- What it does: Counts rows that match a condition
+-- Why use it: Filters results before counting
+-- Syntax: SELECT COUNT(*) FROM table_name WHERE condition;
 __________________________________________________________________________
 -- Problem:
-  -- Management wants to update Bob's salary
+  -- Management wants to know how many employees are in IT
 
 --Solution:
-  UPDATE employees
-    SET salary = 65000
-    WHERE first_name = 'Bob' AND last_name = 'Smith';
-
--- Expected Result:
-  -- id            1       2          3            4        5
-  -- first_name  Alice     Bob       Carol        Dave     Eve
-  -- last_name   Johnson  Smith      Davis        Wilson  Taylor
-  -- department    HR      IT         HR           IT     Finance
-  -- role        Manager  Analyst  Assistant  Developer  Accountant
-  -- salary       75000    65000      50000       70000     NULL
-  -- status      Active   Active    Inactive      Active   Active
-
-__________________________________________________________________________
--- 3 Use Primary Keys When Possible
--- What it does: Targets exactly one row
--- Why use it: IDs are unique and safest
--- Best Practice: Prefer id over names
-__________________________________________________________________________
--- Problem:
-  -- Management wants to activate Carol
-
--- Solution:
-  UPDATE employees 
-    SET status = 'Active'
-    WHERE id = 3;
-
--- Expected Result:
-  -- id            1       2          3          4          5
-  -- first_name  Alice     Bob       Carol       Dave       Eve
-  -- last_name   Johnson  Smith      Davis      Wilson     Taylor
-  -- department    HR      IT         HR         IT       Finance
-  -- role        Manager  Analyst  Assistant  Developer  Accountant
-  -- salary       75000    60000      50000     65000       NULL
-  -- status      Active   Active    Active      Active     Active
-
-__________________________________________________________________________
--- 4 Handle NULLs Correctly
--- What it does: Safely filters rows with missing values
--- Why use it: NULL does NOT work
--- Rule: Always us IS NULL or IS NOT NULL
-__________________________________________________________________________
--- Problem:
-  -- Management remove employees without a salary
-
--- Solution:
-  DELETE FROM employees
-    WHERE salary IS NULL;
-
--- Expected Result:
-  -- id            1       2          3          4
-  -- first_name  Alice     Bob       Carol      Dave
-  -- last_name   Johnson  Smith      Davis     Wilson
-  -- department    HR      IT         HR         IT
-  -- role        Manager  Analyst  Assistant  Developer
-  -- salary       75000    60000      50000     65000
-  -- status      Active   Active    Inactive   Active
-
-__________________________________________________________________________
--- 5 Protect Yourself from Full-Table Updates
--- What it does: Prevents updating every row accidentally
--- Why use it: Missing WHERE affects ALL rows
-__________________________________________________________________________
--- Problem: 
-  -- Management only wants to set works in the IT department as inactive
-
--- Solution: 
-  UPDATE employees
-    SET status = 'Inactive'
+  SELECT COUNT(*) AS ITEmployees
+    FROM employees
     WHERE department = 'IT';
 
--- Expected Results:
-  -- id            1       2          3          4          5
-  -- first_name  Alice     Bob       Carol       Dave       Eve
-  -- last_name   Johnson  Smith      Davis      Wilson     Taylor
-  -- department    HR      IT         HR         IT       Finance
-  -- role        Manager  Analyst  Assistant  Developer  Accountant
-  -- salary       75000    60000      50000     65000      NULL
-  -- status      Active   Inactive    Inactive   Inactive      Active
-
-__________________________________________________________________________
--- 6 Use LIMIT (When Supported) for Extra Safety
--- What it does: Limits the number of affected rows
--- Why use it: Acts as a safety brake
--- Note: Not supported in all databases
-__________________________________________________________________________
--- Problem:
-  -- Management wants to delete on one inactive employee
-
---Solution:
-  DELETE FROM employees
-    WHERE status = 'Inactive'
-    LIMIT 1;
-
 -- Expected Result:
-  -- id            1       2          4          5
-  -- first_name  Alice     Bob       Dave       Eve
-  -- last_name   Johnson  Smith     Wilson     Taylor
-  -- department    HR      IT         IT       Finance
-  -- role        Manager  Analyst  Developer  Accountant
-  -- salary       75000    60000     65000       NULL
-  -- status      Active   Active    Active      Active
+  -- ITEmployees  2
 
 __________________________________________________________________________
--- 3 Wrap Risky Writes in Transactions (Advanced Safety)
--- What it does: Allows rollback if something goes wrong
--- Why use it: Ultimate protection in production systems
+-- 3 COUNT Distinct Values
+-- What it does: Counts unique values in a column
+-- Why use it: Avoid counting duplicates
+-- Syntax: SELECT COUNT(DISTINCT column_name) FROM table_name;
 __________________________________________________________________________
 -- Problem:
-  -- Management wants to activate Carol
+  -- Management wants to know how many different departments exist
 
 -- Solution:
-  BEGIN TRANSACTION;
-  DELETE FROM employees
-    WHERE department = 'Finance';
-
-  ROLLBACK;
-
-  COMMIT;
+  SELECT COUNT(DISTINCT department) AS UniqueDepartments 
+    FROM employees;
 
 -- Expected Result:
-  -- id            1       2          3          4
-  -- first_name  Alice     Bob       Carol       Dave
-  -- last_name   Johnson  Smith      Davis      Wilson
-  -- department    HR      IT         HR         IT
-  -- role        Manager  Analyst  Assistant  Developer
-  -- salary       75000    60000      50000     65000
-  -- status      Active   Active    Inactive    Active
+  -- UniqueDepartments  3
+
+__________________________________________________________________________
+-- 4 COUNT with NULLs
+-- What it does: COUNT(column) ignores NULLs
+-- Why use it: Only counts rows with data in the column
+-- Syntax: SELECT COUNT(column_name) FROM table_name;
+__________________________________________________________________________
+-- Problem:
+  -- Management wants to know how many employees have a salary recorded
+
+-- Solution:
+  SELECT COUNT(salary) AS EmployeeWithSalary
+    FROM employees;
+
+-- Expected Result:
+  -- EmployeeWithSalary  4
+
+__________________________________________________________________________
+-- 5 COUNT with WHERE and NULLs
+-- What it does: Counts rows that meet a condition and handles NULLs
+-- Why use it: SELECT COUNT(column_name) FROM table_name WHERE column_name
+  -- IS NULL;
+__________________________________________________________________________
+-- Problem: 
+  -- Management wants to know how many employees do NOT have a salary
+
+-- Solution: 
+  SELECT COUNT(*) AS EmployeesMissingSalary
+    FROM employees
+    WHERE salary IS NULL;
+
+-- Expected Results:
+  -- EmployeeMissingSalary  1
+
+__________________________________________________________________________
+-- 6 COUNT with Multiple Conditions
+-- What it does: Counts rows matching multiple criteria
+-- Why use it: Allows precise metrics and reporting
+-- Syntax: SELECT COUNT(*) FROM table_name WHERE condition1 AND conditon2;
+__________________________________________________________________________
+-- Problem:
+  -- Management wants to know how many active employees are in HR
+
+--Solution:
+  SELECT COUNT(*) AS ActiveHR
+    FROM employees
+    WHERE department = 'HR' status = 'Active';
+
+-- Expected Result:
+  -- ActiveHR  1
