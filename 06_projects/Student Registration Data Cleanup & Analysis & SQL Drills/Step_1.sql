74bbc2d6c2d6885c1a68cc3da3144d1c07888932
Alexandria Marbury <93114669+Dree0313@users.noreply.github.com>
2026-03-25 14:06:16 -0700

    Update Step_1.sql

--- 06_projects/Student Registration Data Cleanup & Analysis & SQL Drills/Step_1.sql
+++ 06_projects/Student Registration Data Cleanup & Analysis & SQL Drills/Step_1.sql
@@ -21,6 +21,11 @@ SELECT *
 FROM registrations
 WHERE term IS 'Fall2025';
 
+-- This query was incorrect due to my use of IS instead of =. Correct query below:
+
+SELECT *
+FROM registrations
+WHERE term = 'Fall2025';
 
 --🟡 3. Enrollment Patterns (Medium)
 
@@ -34,32 +39,60 @@ SELECT COUNT *
 FROM registrations
 WHERE 
 
-🟡 4. Duplicate Detection (Medium → Hard)
+-- I was unable to determine the correct query for this scenario. 
+-- I should have selected student_id
+-- My usage of COUNT * was also incorrect. It should have been COUNT(*)
+-- Instead of using WHERE, I should have used GROUP BY
+-- Below is the correct query:
 
-Scenario:
-You suspect duplicates exist but need proof.
+SELECT student_id, COUNT(*) AS total_registration
+FROM registrations
+GROUP BY student_id;
 
-Task:
-Identify combinations of:
+-- This works because it selects the student's ID and counts all rows from the table registrations
+-- It then groups by the student ID listing which means that it groups rows with the same value
+  -- a single row.
 
-student_id
-course_id
-term
 
-that appear more than once.
+--🟡 4. Duplicate Detection (Medium → Hard)
 
-(This is where interviewers start paying close attention.)
+--Scenario:
+--You suspect duplicates exist but need proof.
 
-🔴 5. Full Duplicate Investigation (Hard)
+--Task:
+--Identify combinations of:
 
-Scenario:
-Now the department wants to see the actual bad data so they can review it.
+--student_id
+--course_id
+--term
 
-Task:
-Retrieve all rows that are duplicates based on:
+--that appear more than once.
 
-student_id
-course_id
-term
+SELECT student_id, course_id, term, COUNT(*) AS total_registration
+FROM registrations
+GROUP BY;
 
-(Not just counts—the actual records.)
+--I discontinued this problem because I thought that I was unable to group by more than one column.
+--If I had not stopped, I likely would have added student_id, course_id, and term and ended from there.
+--This would not have been correct because I would need to add a HAVING clause that would count all
+  -- respective column rows that already exist
+-- The correct query is below:
+
+SELECT student_id, course_id, term, COUNT(*) AS total_registration
+FROM registrations
+GROUP BY student_id, course_id, term
+HAVING COUNT(*) > 1;
+
+--🔴 5. Full Duplicate Investigation (Hard)
+
+--Scenario:
+--Now the department wants to see the actual bad data so they can review it.
+
+--Task:
+--Retrieve all rows that are duplicates based on:
+
+--student_id
+--course_id
+--term
+
+--(Not just counts—the actual records.)
